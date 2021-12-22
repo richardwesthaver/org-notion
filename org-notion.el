f763cd5c8da8d199f90bd36954cfb8edd07ba6e6
ellis <ellis@rwest.io>
2021-12-21 20:10:24 -0500

    eldev/ert setup and work on org-notion-find-token

--- org-notion.el
+++ org-notion.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2021  ellis
 ;; 
 ;; Author: ellis <ellis@rwest.io>
+;; Version: 0.1.0
 ;; Keywords: wp, extensions
 ;; 
 ;; This program is free software; you can redistribute it and/or modify
@@ -23,10 +24,10 @@
 ;; 
 ;; 
 ;;; Code:
-(require 'url)
-(require 'json)
+(defconst org-notion-host "api.notion.com"
+  "FQDN of Notion API. This is used to create an `auth-source' entry.")
 
-(defconst org-notion-endpoint "https://api.notion.com/v1/"
+(defconst org-notion-endpoint (format "https://%s/v1/" org-notion-host)
   "URL of Notion API endpoint")
 
 (defconst org-notion-version "2021-08-16"
@@ -47,14 +48,30 @@ interactive prompt if token isn't found."
   "Notion integration token used to authenticate requests.
 You can generate one at URL `https://www.notion.so/my-integrations'.")
 
-;; TODO 2021-12-19: check `org-notion-use-auth-source', if 't' query
-;; for token, elif 'nil' or token missing, prompt user for input.
+(defvar org-notion-page-size 100
+  "Default count of results returned by Notion. Additional calls
+will be made to collect all results using the 'start_cursor' parameter. Maximum value is 100.")
+
 (defun org-notion-find-token ()
   "Provision VAR `org-notion-token'.
 If `org-notion-use-auth-source' is 't' check auth-source
 first. If 'nil' or token is missing, prompt for token."
   (interactive)
-  (setq org-notion-token (read-passwd "Notion API Token: ")))
+  (if (and org-notion-use-auth-source
+	   (not org-notion-token))
+      (let ((found (nth 0 (auth-source-search
+			   :max 1
+			   :host org-notion-host
+			   :require '(:secret)
+			   :create t))))
+	(setq org-notion-token (while found
+				 (let ((sec (plist-get found :secret)))
+				   (if (functionp sec)
+				       (funcall sec)
+				     sec))
+				 ))))
+  (unless org-notion-token
+    (read-passwd "Notion API Token: ")))
   
 (defun org-notion-get-current-user ()
   "Retrieve the bot user associated with the current
@@ -66,7 +83,7 @@ first. If 'nil' or token is missing, prompt for token."
 	(url-request-extra-headers
 	 `(("Authorization" . ,(concat "Bearer " org-notion-token))
 	   ("Notion-Version" . ,org-notion-version))))
-    (url-retrieve (concat org-notion-endpoint "users/me") (lambda (status) (switch-to-buffer (current-buffer))))))
+    (url-retrieve (concat org-notion-endpoint "users/me") (lambda (_status) (switch-to-buffer (current-buffer))))))
 
 (defun org-notion-get-users ()
   "Get all users in the Notion workspace. This will return a 403
@@ -79,7 +96,10 @@ enabled."
 	(url-request-extra-headers
 	 `(("Authorization" . ,(concat "Bearer " org-notion-token))
 	   ("Notion-Version" . ,org-notion-version))))
-    (url-retrieve (concat org-notion-endpoint "users") (lambda (status) (switch-to-buffer (current-buffer))))))
+    (url-retrieve (concat org-notion-endpoint "users") (lambda (_status) (switch-to-buffer (current-buffer))))))
+
+(defun org-notion-search (query &optional params)
+  "Search the Notion workspace using QUERY and optional PARAMS")
 
 (provide 'org-notion)
 ;;; org-notion.el ends here
