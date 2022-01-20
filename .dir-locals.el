e6752027e7e7a9a903bfecbfef5481a0168b0edd
ellis <ellis@rwest.io>
2022-01-20 10:59:23 -0500

    dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((compile-command . "make all"))))
+((nil . ((compile-command . "make all")))
+ (org-mode . ((time-stamp-pattern . "4/#\\+DATE: %Y-%02m-%02d$")
+	      (eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
