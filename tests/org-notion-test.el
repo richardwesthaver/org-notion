ce02e5c950f20e6b7c59dfd5e2a17a577bd1863b
ellis <ellis@rwest.io>
2022-12-18 21:14:14 -0500

    reverted tests

--- tests/org-notion-test.el
+++ tests/org-notion-test.el
@@ -35,8 +35,49 @@
 (ert-deftest consts-ok ()
   (should (equal org-notion-version "2021-08-16"))
   (should (equal org-notion-host "api.notion.com"))
+  (should (equal org-notion-endpoint "https://api.notion.com/v1/")))
+
+(ert-deftest no-auth-source-ok ()
+  (let ((org-notion-use-auth-source nil))
+    (should (equal (org-notion-token "token-test") "token-test"))))
+
+;; FIXME 2022-01-02: currently requires ~/.authinfo.gpg
+(ert-deftest auth-source-ok ()
+  (let ((org-notion-use-auth-source t))
+    (should (org-notion-token))))
+
+(ert-deftest current-user-ok () (should (org-notion-get-current-user)))
+
+(ert-deftest users-ok () (should (org-notion-get-users)))
+
+(ert-deftest user-from-json-ok ()
+  (let ((results (get-results (json-read-file "json/users.json"))))
+    (org-notion-from-json (org-notion-user) (elt results 0))
+    (should (org-notion-from-json (org-notion-user) (elt results 0)))))
+
+(ert-deftest user-to-json-ok ()
+  (should (org-notion-to-json (org-notion-user))))
+
+(ert-deftest user-to-org-ok ()
+  (should (org-notion-to-org (org-notion-user) 'heading)))
+
+(ert-deftest search-ok () (should (org-notion-search "org-notion")))
+
+;; org-mode tests
+(ert-deftest to-org-time-ok ()
+  (set-time-zone-rule t)
+  (should (string= (org-notion-to-org-time "2022-01-09T08:59:15.000Z") "2022-01-09 08:59:15")))
+
+(ert-deftest from-org-time-ok ()
+  (set-time-zone-rule t)
+  (should (string= (org-notion-from-org-time "2022-01-09 08:59:15") "2022-01-09T08:59:15+0000")))
+
+(ert-deftest id-at-point-ok ()
+  (with-temp-buffer
     (insert-file-contents "mock.org")
-    (let (())
-      (should (equal )))))
+    (let ((id "64adb50d17394203a31265641aeaeb8e")
+	  (pom (point-min)))
+      (should (equal (org-notion-id-at-point pom) id)))))
+
 (provide 'org-notion-test)
 ;;; org-notion-test.el ends here
